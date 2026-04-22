9245c1a03a7d332c2fa83e26257f8e86d1f9344d
dgrector55428@gmail.com <dgrector55428@gmail.com>
2026-04-21 19:03:16 -0500

    milestone.1 add Bambu printer configuration options

--- api/PrinterDashboard.Api/Configuration/BambuOptions.cs
+++ api/PrinterDashboard.Api/Configuration/BambuOptions.cs
@@ -2,7 +2,13 @@ namespace PrinterDashboard.Api.Configuration;
 
 public class BambuOptions
 {
-    public const string SectionName = "Bambu";
+    public const string SectionName = "BambuPrinter";
+
     public string Host { get; set; } = string.Empty;
+
+    public int Port { get; set; } = 8883;
+
+    public string Username { get; set; } = "bblp";
+
     public string AccessCode { get; set; } = string.Empty;
 }
